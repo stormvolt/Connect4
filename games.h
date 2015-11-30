bd994f2d96c94fad0766db1200a03640a47176fa
luis <luisnu97@hotmail.com>
2015-11-30 15:16:17 -0500

    games.h

---
+++ games.h
@@ -0,0 +1,17 @@
+#ifndef GAMES_H
+#define GAMES_H
+
+#include "graphics.h"
+#include "graphics.cpp"
+
+
+void startClassic();
+void startPop();
+void startRow();
+void startFive();
+void startQuick();
+void showScores();
+
+#endif
+
+
